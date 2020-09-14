bfc6418140e7c2aa0f58b47b26fb3350df4a647b
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-09-14 17:37:40 +1000

    add player's move validation

--- lesson_5/oo_ttt.js
+++ lesson_5/oo_ttt.js
@@ -14,6 +14,10 @@ class Square {
   setMarker(marker) {
     this.marker = marker;
   }
+
+  isUnused() {
+    return this.marker === Square.UNUSED_SQUARE;
+  }
 }
 
 class Board {
@@ -49,6 +53,11 @@ class Board {
   markSquareAt(key, marker) {
     this.squares[key].setMarker(marker);
   }
+
+  unusedSquares() {
+    let keys = Object.keys(this.squares);
+    return keys.filter((key) => this.squares[key].isUnused());
+  }
 }
 
 class Row {
@@ -65,12 +74,6 @@ class Player {
 
   getMarker() {
     return this.marker;
-  }
-
-  play() {
-    //STUB
-    // We need a way for each player to play the game.
-    // Do we need access to the board?
   }
 }
 
@@ -103,13 +106,10 @@ class TTTGame {
       this.board.display();
 
       this.humanMoves();
-      this.board.display();
       if (this.gameOver()) break;
 
       this.computerMoves();
-      this.board.display();
       if (this.gameOver()) break;
-      break;
     }
 
     this.displayResults();
@@ -133,12 +133,11 @@ class TTTGame {
     let choice;
 
     while (true) {
-      choice = readline.question("Choose a square between 1 and 9: ");
+      let validChoices = this.board.unusedSquares();
+      const prompt = `Choose a square(${validChoices.join(", ")}): `;
+      choice = readline.question(prompt);
 
-      let integerValue = parseInt(choice, 10);
-      if (integerValue >= 1 && integerValue <= 9) {
-        break;
-      }
+      if (validChoices.includes(choice)) break;
 
       console.log("Sorry, that's not a valid choice.");
       console.log("");
@@ -148,7 +147,13 @@ class TTTGame {
   }
 
   computerMoves() {
-    let choice = Math.floor(9 * Math.random() + 1);
+    let validChoices = this.board.unusedSquares();
+    let choice;
+
+    do {
+      choice = Math.floor(9 * Math.random() + 1).toString();
+    } while (!validChoices.includes(choice));
+
     this.board.markSquareAt(choice, this.computer.getMarker());
   }
 
